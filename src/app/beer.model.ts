d175cff58229fe6257b6d9178ac652337b1ecef7
Matt <mmuhr88@gmail.com>
2017-04-17 16:34:08 -0700

    updated beer object to account for objects missing properties from API

--- src/app/beer.model.ts
+++ src/app/beer.model.ts
@@ -9,22 +9,14 @@ export class Beer {
   label_url: string;
   description: string;
   type_name: string;
-  // constructor(
-  //   public id: string,
-  //   public name: string,
-  //   public abv: string,
-  //   public ibu: string,
-  //   public label_url: string,
-  //   public description: string,
-  //   public type_name: string) {
-  // }
+
   constructor(data: any) {
-    this.id = data.id;
-    this.name = data.name;
-    this.abv = data.abv;
-    this.ibu = data.ibu;
-    this.label_url = data.label_url;
-    this.description = data.description;
-    this.type_name = data.type_name;
+    this.id = data.id  || null;
+    this.name = data.name || null;
+    this.abv = data.abv || null;
+    this.ibu = data.ibu || null;
+    this.label_url = data.label_url || null;
+    this.description = data.description || null;
+    this.type_name = data.type_name || null;
   }
 }
