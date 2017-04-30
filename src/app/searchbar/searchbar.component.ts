a4f827d7b9922db3b54074feee46f2f6fef036d8
Robert Murray <work@robertmurray.me>
2017-04-30 13:53:23 -0700

    added some comments and moved the model file into a model directory.

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, EventEmitter,
  Output } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Beer } from '../beer.model';
+import { Beer } from '../models/beer.model';
 
 @Component({
   selector: 'app-searchbar',
@@ -13,7 +13,8 @@ export class SearchbarComponent implements OnInit {
 
   loading: boolean;
   http: Http;
-  link = `http://the-ipa-api.herokuapp.com/ipa-api/search?name=`;
+  // no need to make link a component level var. it only lives in the scope of performSearch.
+  // link = `http://the-ipa-api.herokuapp.com/ipa-api/search?name=`;
 
   constructor(http: Http) {
     this.http = http;
@@ -23,16 +24,21 @@ export class SearchbarComponent implements OnInit {
     this.loading = false;
   }
 
+  // this call would be best placed in a service and injected into this component.
   performSearch(query: HTMLInputElement): void {
     this.loading = true;
-    const apiLink = this.link + query.value;
+    // const apiLink = this.link + query.value;
+    // this is probably a better way to do this.
+    const label_url = 'http://png-4.vector.me/files/images/6/6/668633/duff_beer_thumb.png'
+    const apiLink = `http://the-ipa-api.herokuapp.com/ipa-api/search?name=${query.value}`;
     this.http.get(apiLink)
     .subscribe((res: Response) => {
       const resultArray = res.json();
       const beerArray = [];
       for (let i = 0; i < resultArray.length; i++) {
           const beer = new Beer(resultArray[i]);
-          if (beer.label_url !== 'http://png-4.vector.me/files/images/6/6/668633/duff_beer_thumb.png') {
+          // this is cleaner than having the label_url here.
+          if (beer.label_url !== label_url) {
              beerArray.push(beer);
           }
           this.loading = false;
